73d17c748aa172c5908aefbadb01351ce44ef18d
szhang245 <szhang245@myseneca.ca>
2025-04-16 15:46:56 -0400

    szhang245 server.js 1

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
-*  SEC Final Assignment
+*  SEC Group 1 Final Assignment
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
